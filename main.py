f37ad6e47e1949e6f6c35bf66bb0b4bcdd578a06
alzencastillo123 <85831138+alzencastillo123@users.noreply.github.com>
2021-07-08 23:15:16 +0800

    Update main.py
    Alzen gwapo

--- main.py
+++ main.py
@@ -76,6 +76,18 @@ class Calculator:
         frame.pack(expand=True, fill="both")
         return frame
 
+    def create_digit_buttons(self):   
+        for digit, grid_value in self.digits.items():
+            button = tk.Button(self.buttons_frame, text=str(digit), bg = WHITE, fg=LABEL_COLOR, font=DIGITS_FONT_STYLE, borderwidth = 0, command=lambda x=digit: self.add_to_expression(x))
+            button.grid(row=grid_value[0], column=grid_value[1], sticky=tk.NSEW)
+    
+    def create_operator_buttons(self):
+        i = 1
+        for operator,symbol in self.operations.items():
+            button = tk.Button(self.buttons_frame, text=symbol, bg=OFF_WHITE, fg=LABEL_COLOR, font= DEFAULT_FONT_STYLE, borderwidth = 0, command=lambda x=operator: self.append_operator(x))
+            button.grid(row =i, column = 4, sticky=tk.NSEW)
+            i += 1
+
     def create_buttons_frame(self):
         frame = tk.Frame(self.window)
         frame.pack(expand=True, fill="both")
