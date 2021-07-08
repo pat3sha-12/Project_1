961fca8e7b34680a6e22e25db20623b8f06cbd33
pat3sha-12 <84178289+pat3sha-12@users.noreply.github.com>
2021-07-08 22:49:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -41,6 +41,10 @@ class Calculator:
             self.buttons_frame.columnconfigure(x, weight = 1)
         
         self.bind_keys()
+        self.create_digit_buttons()
+        self.create_operator_buttons()
+        self.create_special_buttons()
+        self.bind_keys()
     
     def bind_keys(self):
         self.window.bind("<Return>", lambda event: self.evaluate())
@@ -50,8 +54,7 @@ class Calculator:
         for key in self.operations:
             self.window.bind(key, lambda event, operator=key: self.append_operator(operator))
             
-            
-        def create_special_buttons(self):
+    def create_special_buttons(self):
         self.create_clear_button()
         self.create_equals_button()
         self.create_square_button()
